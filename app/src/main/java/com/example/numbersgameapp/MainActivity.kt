8c099ac3030c3bd73a325e736b819bebf721ddef
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-26 22:12:18 +0300

    The break gives some great results

--- app/src/main/java/com/example/numbersgameapp/MainActivity.kt
+++ app/src/main/java/com/example/numbersgameapp/MainActivity.kt
@@ -6,39 +6,55 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 lateinit var guessList: ArrayList<String>
-lateinit var guessBtn: Button
 lateinit var userGuess: EditText
 lateinit var recyclerView: RecyclerView
+lateinit var btn: Button
 var numberOfGuesses = 3
+val random = Random.nextInt(11)
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         guessList = arrayListOf()
-        val random = Random.nextInt(11)
+
         userGuess = findViewById(R.id.etNumGuessed)
-        guessBtn = findViewById(R.id.guessBtn)
-        guessBtn.setOnClickListener{
-            check(random)
-            userGuess.setText("")
+        btn = findViewById(R.id.guessBtn)
+        btn.setOnClickListener{
+            check()
+
             updateRecyclerView()
         }
     }
 
-    private fun check(random: Int){
+    private fun check(){
+
+        if (userGuess.text.isEmpty()){
+            Snackbar.make(cl, "You must enter a number", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+
         val guessToInt = userGuess.text.toString().toInt()
         if (random == guessToInt){
             guessList.add("That's right! it is $random")
+            userGuess.isEnabled = false
         }
         else {
-            guessList.add("You guessed $guessToInt\nYou have ${--numberOfGuesses} guesses left")
+            var plural = "guesses"
+            if (--numberOfGuesses == 1) plural = "guess"
+            guessList.add("You guessed $guessToInt\nYou have $numberOfGuesses $plural left")
         }
-        if (numberOfGuesses == 0)
+        if (numberOfGuesses == 0) {
             guessList.add("The number was $random")
+            userGuess.isEnabled = false
+        }
+        userGuess.setText("")
     }
 
     private fun updateRecyclerView(){
